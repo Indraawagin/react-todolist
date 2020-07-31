399a56157d14b1233a8975f6f93a898a04c810da
indraawagin <indraawagin@gmail.com>
2020-07-31 23:52:14 +0800

    Hooks Todolist

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = (props) => {
   return (
     <section className="header">
-      <button className="header-btn main-black-color">Add</button>
+      <button
+        className="header-btn main-black-color"
+        onClick={props.showAddToogle}
+      >
+        Add
+      </button>
       <h1 className="header-title">Todo List</h1>
       <button className="header-btn main-red-color">Clear</button>
     </section>
   );
 };
 
+Header.propTypes = {
+  showAddToogle: PropTypes.func.isRequired,
+};
 export default Header;
--- src/Pages/TodoList.jsx
+++ src/Pages/TodoList.jsx
@@ -5,21 +5,39 @@ import TodoForm from "../Components/TodoForm";
 import Todos from "../Components/Todos";
 
 const TodoList = (props) => {
+  //TODO: Hooks Todo List
   const [todos, setTodos] = useState([
     { text: "Learning React" },
     { text: "Learning React Hooks" },
     { text: "Learning styling in React" },
   ]);
 
+  //TODO: Hooks Show Add Todo Form
+  const [showAdd, setShowAdd] = useState(false);
+
+  //TODO: <TodoForm addTodo={addTodo} />
   const addTodo = (value) => {
     const addedTodo = [...todos, { text: value }];
     setTodos(addedTodo);
   };
 
+  //Todo: <Header showAddToogle={showAddToggle} />
+  const showAddToggle = () => setShowAdd(!showAdd);
+
+  //Todo: {addForm}
+  let addForm = null;
+  if (showAdd) {
+    addForm = (
+      <div>
+        <TodoForm addTodo={addTodo} />
+      </div>
+    );
+  }
+
   return (
     <Paper>
-      <Header />
-      <TodoForm addTodo={addTodo} />
+      <Header showAddToogle={showAddToggle} />
+      {addForm}
       <Todos todos={todos} />
     </Paper>
   );
